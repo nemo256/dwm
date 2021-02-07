2018d0da172a67796fef092ad6d4810f18a45a30
nemo256 <neggazimedlamine@gmail.com>
2021-02-07 18:37:17 +0100

    Updated icons sizes!

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const unsigned int gappx     = 5;        /* gaps size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=20:style=bold", "fontawesome:size=20:style=bold" };
+static const char *fonts[]          = { "monospace:size=20:style=bold", "fontawesome:size=22:style=bold" };
 static const char dmenufont[]       = "monospace:size=20:style=bold";
 //background color
 static const char col_gray1[]       = "#222222";
