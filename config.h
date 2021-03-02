a663c90dd0c320180396ccc36c8684bd3797b6ce
nemo256 <neggazimedlamine@gmail.com>
2021-03-02 20:10:29 +0100

    Fixed key number 5 binding!

--- config.h
+++ config.h
@@ -208,7 +208,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
+	//TAGKEYS(                        XK_5,                      4)
 	//TAGKEYS(                        XK_6,                      5)
 	//TAGKEYS(                        XK_7,                      6)
 	//TAGKEYS(                        XK_8,                      7)
