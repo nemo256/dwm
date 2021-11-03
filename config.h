cd718ad7bdaa92b8fa928609972bbed5f782d080
Amine Neggazi <neggazimedlamine@gmail.com>
2021-11-03 20:21:46 +0100

    Added Victor Mono Font...

--- config.h
+++ config.h
@@ -8,8 +8,8 @@ static const unsigned int gappx     = 5;        /* gaps size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Bitstream Vera Sans Mono:size=20:style=bold", "fontawesome:size=20:style=bold" };
-static const char dmenufont[]       = "Bitstream Vera Sans Mono:size=20:style=bold";
+static const char *fonts[]          = { "Victor Mono Bold:size=20:style=bold", "fontawesome:size=20:style=bold" };
+static const char dmenufont[]       = "Victor Mono Bold:size=20:style=bold";
 //background color
 static const char col_black[]       = "#000000";
 //inactive window border color
